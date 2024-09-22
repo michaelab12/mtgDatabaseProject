7f001ae72d6aeec56c5238036e037ab3a70204d8
mdv7374 <mdv7374@rit.edu>
2024-09-22 19:38:18 -0400

    made changes to the display

--- js/cardPage.js
+++ js/cardPage.js
@@ -19,11 +19,24 @@ async function getCards(id){
 }
 //helper function used to set all the needed values
 function setValues(json){
+    document.title = json.name;
     let cardName = document.getElementById('cardName');
     cardName.textContent = json.name;
     let cardImage = document.getElementById('cardImage');
     let image = '<img src="' + json.image_uris.normal + '"width="488" height ="680">';
     cardImage.innerHTML = image;
+    let typeLine = document.getElementById('typeLine');
+    typeLine.textContent = json.type_line;
+    let desc = document.getElementById('description');
+    desc.textContent = json.oracle_text;
+    //only do if it has power and toughness
+    if(json.power == null || json.toughness == null){
+        document.getElementById('powerToughness').remove();
+    }
+    else{
+        let powerToughness = document.getElementById('powerToughness');
+        powerToughness.textContent = json.power + '/' + json.toughness;
+    }
 }
 
 //helper function 
